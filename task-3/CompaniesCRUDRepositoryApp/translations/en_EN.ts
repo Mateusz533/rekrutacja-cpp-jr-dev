5a6c200b41fb0926da8c6c00d581a1cd94c99276
Mateusz Frejlich <mateuszfrejlich533@gmail.com>
2024-01-23 17:00:50 +0100

    fix config

--- task-3/CompaniesCRUDRepositoryApp/translations/en_EN.ts
+++ task-3/CompaniesCRUDRepositoryApp/translations/en_EN.ts
@@ -1,138 +1,182 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
-  <context>
-    <name>CompaniesCRUDRepositoryApp</name>
-    <message>
-      <location filename="../companiescrudrepositoryapp.cpp"/>
-      <source>Fail to load translation!</source>
-      <translation>Fail to load translation!</translation>
-    </message>
-    <message>
-      <location filename="../companiescrudrepositoryapp.cpp"/>
-      <source>Invalid form data!</source>
-      <translation>Invalid form data!</translation>
-    </message>
-    <message>
-      <location filename="../companiescrudrepositoryapp.cpp"/>
-      <source>Duplicated ID!</source>
-      <translation>Duplicated TIN!</translation>
-    </message>
-    <message>
-      <location filename="../companiescrudrepositoryapp.cpp"/>
-      <source>Incorrect file!</source>
-      <translation>Incorrect file!</translation>
-    </message>
-    <message>
-      <location filename="../companiescrudrepositoryapp.cpp"/>
-      <source>File data is incorrect!</source>
-      <translation>File data is incorrect!</translation>
-    </message>
-  </context>
-  <context>
+<context>
     <name>CSVPersistanceController</name>
     <message>
-      <location filename="../csvpersistancecontroller.cpp"/>
-      <source>Select file</source>
-      <translation>Select file</translation>
+        <source>Select file</source>
+        <translation type="vanished">Select file</translation>
     </message>
-  </context>
-  <context>
+</context>
+<context>
+    <name>CompaniesCRUDRepositoryApp</name>
+    <message>
+        <location filename="../companiescrudrepositoryapp.cpp" line="90"/>
+        <source>Fail to load translation!</source>
+        <translation>Fail to load translation!</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.cpp" line="103"/>
+        <location filename="../companiescrudrepositoryapp.cpp" line="126"/>
+        <source>Invalid form data!</source>
+        <translation>Invalid form data!</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.cpp" line="107"/>
+        <location filename="../companiescrudrepositoryapp.cpp" line="134"/>
+        <source>Duplicated ID!</source>
+        <translation>Duplicated TIN!</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.cpp" line="167"/>
+        <location filename="../companiescrudrepositoryapp.cpp" line="174"/>
+        <source>Incorrect file!</source>
+        <translation>Incorrect file!</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.cpp" line="183"/>
+        <location filename="../companiescrudrepositoryapp.cpp" line="186"/>
+        <source>File data is incorrect!</source>
+        <translation>File data is incorrect!</translation>
+    </message>
+</context>
+<context>
     <name>CompaniesCRUDRepositoryAppClass</name>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Repozytorium firm</source>
-      <translation>Companies Repository</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="32"/>
+        <source>Repozytorium firm</source>
+        <translation>Companies Repository</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Nazwa</source>
-      <translation>Name</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="81"/>
+        <source>Nazwa</source>
+        <translation>Name</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>NIP</source>
-      <translation>TIN</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="91"/>
+        <source>NIP</source>
+        <translation>TIN</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Nr Tel.</source>
-      <translation>Tel. No.</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="98"/>
+        <source>111-22-33-444</source>
+        <translation>111-22-33-444</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Adres</source>
-      <translation>Address</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="105"/>
+        <source>Nr Tel.</source>
+        <translation>Tel. No.</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>E-mail</source>
-      <translation>E-mail</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="112"/>
+        <source>123456789</source>
+        <translation>123456789</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Dodaj</source>
-      <translation>Add</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="119"/>
+        <location filename="../companiescrudrepositoryapp.ui" line="269"/>
+        <source>Adres</source>
+        <translation>Address</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Aktualizuj</source>
-      <translation>Update</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="129"/>
+        <source>E-mail</source>
+        <translation>E-mail</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Usuń</source>
-      <translation>Delete</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="136"/>
+        <source>example@mysite.com</source>
+        <translation>example@mysite.com</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Nazwa firmy</source>
-      <translation>Company name</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="174"/>
+        <source>Dodaj</source>
+        <translation>Add</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Numer NIP</source>
-      <translation>TIN number</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="193"/>
+        <source>Aktualizuj</source>
+        <translation>Update</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Numer telefonu</source>
-      <translation>Phone number</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="212"/>
+        <source>Usuń</source>
+        <translation>Delete</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Adres e-mail</source>
-      <translation>E-mail address</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="254"/>
+        <source>Nazwa firmy</source>
+        <translation>Company name</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Plik</source>
-      <translation>File</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="259"/>
+        <source>Numer NIP</source>
+        <translation>TIN number</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Zapisz</source>
-      <translation>Save</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="264"/>
+        <source>Numer telefonu</source>
+        <translation>Phone number</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Zapisz jako ...</source>
-      <translation>Save as ...</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="274"/>
+        <source>Adres e-mail</source>
+        <translation>E-mail address</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Wczytaj</source>
-      <translation>Read</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="294"/>
+        <source>Plik</source>
+        <translation>File</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Zamknij</source>
-      <translation>Close</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="322"/>
+        <source>Zapisz</source>
+        <translation>Save</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Język</source>
-      <translation>Language</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="327"/>
+        <source>Zapisz jako ...</source>
+        <translation>Save as ...</translation>
     </message>
-  </context>
+    <message>
+        <location filename="../companiescrudrepositoryapp.ui" line="332"/>
+        <source>Wczytaj</source>
+        <translation>Read</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.ui" line="337"/>
+        <source>Zamknij</source>
+        <translation>Close</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.ui" line="342"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.ui" line="347"/>
+        <source>Polski</source>
+        <translation>Polish</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.ui" line="303"/>
+        <source>Język</source>
+        <translation>Language</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../csvpersistancecontroller.cpp" line="4"/>
+        <location filename="../csvpersistancecontroller.cpp" line="21"/>
+        <source>Select file</source>
+        <translation>Select file</translation>
+    </message>
+    <message>
+        <location filename="../csvpersistancecontroller.cpp" line="4"/>
+        <location filename="../csvpersistancecontroller.cpp" line="21"/>
+        <source>CSV (*.csv)</source>
+        <translation>CSV (*.csv)</translation>
+    </message>
+</context>
 </TS>
--- task-3/CompaniesCRUDRepositoryApp/translations/pl_PL.ts
+++ task-3/CompaniesCRUDRepositoryApp/translations/pl_PL.ts
@@ -1,138 +1,182 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="pl_PL">
-  <context>
-    <name>CompaniesCRUDRepositoryApp</name>
-    <message>
-      <location filename="../companiescrudrepositoryapp.cpp"/>
-      <source>Fail to load translation!</source>
-      <translation>Nie udało się wczytać tłumaczenia!</translation>
-    </message>
-    <message>
-      <location filename="../companiescrudrepositoryapp.cpp"/>
-      <source>Invalid form data!</source>
-      <translation>Nieprawidłowe dane w formularzu!</translation>
-    </message>
-    <message>
-      <location filename="../companiescrudrepositoryapp.cpp"/>
-      <source>Duplicated ID!</source>
-      <translation>Powtarzający się NIP!</translation>
-    </message>
-    <message>
-      <location filename="../companiescrudrepositoryapp.cpp"/>
-      <source>Incorrect file!</source>
-      <translation>Nieprawidłowy plik!</translation>
-    </message>
-    <message>
-      <location filename="../companiescrudrepositoryapp.cpp"/>
-      <source>File data is incorrect!</source>
-      <translation>Dane w pliku są niepoprawne!</translation>
-    </message>
-  </context>
-  <context>
+<context>
     <name>CSVPersistanceController</name>
     <message>
-      <location filename="../csvpersistancecontroller.cpp"/>
-      <source>Select file</source>
-      <translation>Wybierz plik</translation>
+        <source>Select file</source>
+        <translation type="vanished">Wybierz plik</translation>
     </message>
-  </context>
-  <context>
+</context>
+<context>
+    <name>CompaniesCRUDRepositoryApp</name>
+    <message>
+        <location filename="../companiescrudrepositoryapp.cpp" line="90"/>
+        <source>Fail to load translation!</source>
+        <translation>Nie udało się wczytać tłumaczenia!</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.cpp" line="103"/>
+        <location filename="../companiescrudrepositoryapp.cpp" line="126"/>
+        <source>Invalid form data!</source>
+        <translation>Nieprawidłowe dane w formularzu!</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.cpp" line="107"/>
+        <location filename="../companiescrudrepositoryapp.cpp" line="134"/>
+        <source>Duplicated ID!</source>
+        <translation>Powtarzający się NIP!</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.cpp" line="167"/>
+        <location filename="../companiescrudrepositoryapp.cpp" line="174"/>
+        <source>Incorrect file!</source>
+        <translation>Nieprawidłowy plik!</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.cpp" line="183"/>
+        <location filename="../companiescrudrepositoryapp.cpp" line="186"/>
+        <source>File data is incorrect!</source>
+        <translation>Dane w pliku są niepoprawne!</translation>
+    </message>
+</context>
+<context>
     <name>CompaniesCRUDRepositoryAppClass</name>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Repozytorium firm</source>
-      <translation>Repozytorium firm</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="32"/>
+        <source>Repozytorium firm</source>
+        <translation>Repozytorium firm</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Nazwa</source>
-      <translation>Nazwa</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="81"/>
+        <source>Nazwa</source>
+        <translation>Nazwa</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>NIP</source>
-      <translation>NIP</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="91"/>
+        <source>NIP</source>
+        <translation>NIP</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Nr Tel.</source>
-      <translation>Nr Tel.</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="98"/>
+        <source>111-22-33-444</source>
+        <translation>111-22-33-444</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Adres</source>
-      <translation>Adres</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="105"/>
+        <source>Nr Tel.</source>
+        <translation>Nr Tel.</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>E-mail</source>
-      <translation>E-mail</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="112"/>
+        <source>123456789</source>
+        <translation>123456789</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Dodaj</source>
-      <translation>Dodaj</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="119"/>
+        <location filename="../companiescrudrepositoryapp.ui" line="269"/>
+        <source>Adres</source>
+        <translation>Adres</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Aktualizuj</source>
-      <translation>Aktualizuj</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="129"/>
+        <source>E-mail</source>
+        <translation>E-mail</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Usuń</source>
-      <translation>Usuń</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="136"/>
+        <source>example@mysite.com</source>
+        <translation>example@mysite.com</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Nazwa firmy</source>
-      <translation>Nazwa firmy</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="174"/>
+        <source>Dodaj</source>
+        <translation>Dodaj</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Numer NIP</source>
-      <translation>Numer NIP</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="193"/>
+        <source>Aktualizuj</source>
+        <translation>Aktualizuj</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Numer telefonu</source>
-      <translation>Numer telefonu</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="212"/>
+        <source>Usuń</source>
+        <translation>Usuń</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Adres e-mail</source>
-      <translation>Adres e-mail</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="254"/>
+        <source>Nazwa firmy</source>
+        <translation>Nazwa firmy</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Plik</source>
-      <translation>Plik</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="259"/>
+        <source>Numer NIP</source>
+        <translation>Numer NIP</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Zapisz</source>
-      <translation>Zapisz</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="264"/>
+        <source>Numer telefonu</source>
+        <translation>Numer telefonu</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Zapisz jako ...</source>
-      <translation>Zapisz jako ...</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="274"/>
+        <source>Adres e-mail</source>
+        <translation>Adres e-mail</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Wczytaj</source>
-      <translation>Wczytaj</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="294"/>
+        <source>Plik</source>
+        <translation>Plik</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Zamknij</source>
-      <translation>Zamknij</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="322"/>
+        <source>Zapisz</source>
+        <translation>Zapisz</translation>
     </message>
     <message>
-      <location filename="../companiescrudrepositoryapp.ui"/>
-      <source>Język</source>
-      <translation>Język</translation>
+        <location filename="../companiescrudrepositoryapp.ui" line="327"/>
+        <source>Zapisz jako ...</source>
+        <translation>Zapisz jako ...</translation>
     </message>
-  </context>
+    <message>
+        <location filename="../companiescrudrepositoryapp.ui" line="332"/>
+        <source>Wczytaj</source>
+        <translation>Wczytaj</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.ui" line="337"/>
+        <source>Zamknij</source>
+        <translation>Zamknij</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.ui" line="342"/>
+        <source>English</source>
+        <translation>Angielski</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.ui" line="347"/>
+        <source>Polski</source>
+        <translation>Polski</translation>
+    </message>
+    <message>
+        <location filename="../companiescrudrepositoryapp.ui" line="303"/>
+        <source>Język</source>
+        <translation>Język</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../csvpersistancecontroller.cpp" line="4"/>
+        <location filename="../csvpersistancecontroller.cpp" line="21"/>
+        <source>Select file</source>
+        <translation>Wybierz plik</translation>
+    </message>
+    <message>
+        <location filename="../csvpersistancecontroller.cpp" line="4"/>
+        <location filename="../csvpersistancecontroller.cpp" line="21"/>
+        <source>CSV (*.csv)</source>
+        <translation>CSV (*.csv)</translation>
+    </message>
+</context>
 </TS>
